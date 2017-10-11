7683cdd6c5dca836a624b8acb1c3c8fe2592552b
Brad Qiu <brad.qiu@finisar.com>
2017-10-11 11:35:51 +0800

    Set the project environment

--- ProLEGO/ProLEGO/App_Start/RouteConfig.cs
+++ ProLEGO/ProLEGO/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ProLEGO
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ProLEGO", action = "MainPage", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ProLEGO/ProLEGO/Controllers/ProLEGOController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProLEGO.Controllers
+{
+    public class ProLEGOController : Controller
+    {
+        public ActionResult MainPage()
+        {
+            return View();
+        }
+    }
+}
